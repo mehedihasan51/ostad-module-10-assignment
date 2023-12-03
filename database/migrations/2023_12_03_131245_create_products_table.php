df059539920c4a118605acef7df0c9be6eebc9a3
Mehedi Hasan <mehedihasan314551@gmail.com>
2023-12-03 20:18:57 +0600

    assignment 10

---
+++ database/migrations/2023_12_03_131245_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("product_name");
+            $table->decimal("product_price");
+            $table->string("product_description");
+            $table->integer('quantity')->nullable(); // New column: quantit
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp("update_at")->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
